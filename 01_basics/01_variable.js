b11dddfc82c6c2ec4e74726574fdcb1148958e64
Shrey-Coder-478 <shreychauhan6061@gmail.com>
2025-07-07 15:12:05 +0530

    Update 01_variable.js

--- 01_basics/01_variable.js
+++ 01_basics/01_variable.js
@@ -1,15 +1,15 @@
 const accountId = 144553
-let accountEmail = "hitesh@google.com"
+let accountEmail = "shrey123@gmail.com"
 var accountPassword = "12345"
-accountCity = "Jaipur"
+accountCity = "Ahmedabad"
 let accountState;
 
 // accountId = 2 // not allowed
 
 
-accountEmail = "hc@hc.com"
-accountPassword = "21212121"
-accountCity = "Bengaluru"
+accountEmail = "axis@xyz.com"
+accountPassword = "00000099999"
+accountCity = "Ahmedabad"
 
 console.log(accountId);
 
