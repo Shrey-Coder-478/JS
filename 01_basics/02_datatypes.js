e88017ac5bad15dfdc40dd477e397338ee5f47b5
Shrey-Coder-478 <shreychauhan6061@gmail.com>
2025-07-07 15:12:48 +0530

    Update 02_datatypes.js

--- 01_basics/02_datatypes.js
+++ 01_basics/02_datatypes.js
@@ -6,11 +6,11 @@ console.log(3
     +
      3) // code readability should be high
 
-console.log("Hitesh")
+console.log("Shrey")
 
 
-let name = "hitesh"
-let age = 18
+let name = "shrey"
+let age = 21
 let isLoggedIn = false
 let state;
 
